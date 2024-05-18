9e4e0b5debb25fd99c29c8766bdd736f25e3f9ab
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-18 17:24:04 +0500

    Add files via upload

---
+++ task26.js
@@ -0,0 +1,18 @@
+"use strict";
+// Task 26
+let variabe_alien = "green";
+// if block runs
+if (variabe_alien === "green") {
+    console.log("Player just earned 5 points for shooting an alien!");
+}
+else {
+    console.log("Player just earned 10 points");
+}
+// else block runs
+variabe_alien = "red";
+if (variabe_alien === "green") {
+    console.log("Player just earned 5 points for shooting an alien!");
+}
+else {
+    console.log("Player just earned 10 points");
+}
---
+++ task26.ts
@@ -0,0 +1,18 @@
+// Task 26
+let variabe_alien :string ="green"
+// if block runs
+if (variabe_alien === "green"){
+    console.log("Player just earned 5 points for shooting an alien!")
+}
+else{console.log("Player just earned 10 points")}
+
+
+
+// else block runs
+variabe_alien = "red"
+if (variabe_alien === "green"){
+    console.log("Player just earned 5 points for shooting an alien!")
+}
+else{console.log("Player just earned 10 points")}
+
+
